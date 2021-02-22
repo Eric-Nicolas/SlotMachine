4c26e84a2b625afd4775ba3b902b5a3ffe4e077d
Eric-Nicolas <randomimus.beakuzz@gmail.com>
2021-02-22 19:15:50 +0100

    Made the script at the second level.

--- main.py
+++ main.py
@@ -10,18 +10,34 @@ WATERMELON = 'watermelon'
 GOLDEN_APPLE = 'golden apple'
 
 
+def has_same_value(seq: list) -> bool:
+    for i in seq:
+        if i != seq[0]:
+            return False
+    return True
+
+
 def main() -> None:
     print("Welcome to the machine slot GravenFruitiiii!")
 
     fruits = [ORANGE, CHERRY, PINEAPPLE, WATERMELON, GOLDEN_APPLE]
-    print(random.choice(fruits))
+    tokens_associated = {
+        ORANGE: 5,
+        CHERRY: 15,
+        PINEAPPLE: 50,
+        WATERMELON: 150,
+        GOLDEN_APPLE: 10000
+    }
 
-    random_fruits = []
-    while len(random_fruits) < 3:
-        fruit = random.choice(fruits)
-        if fruit not in random_fruits:
-            random_fruits.append(fruit)
+    tokens = 0
+
+    # returns k elements of fruits; each item has a probability written in the weights parameter
+    random_fruits = random.choices(fruits, weights=(40, 25, 20, 10, 5), k=3)
     print(random_fruits)
+
+    if has_same_value(random_fruits):
+        print("3x " + random_fruits[0] + " - You win " + str(tokens_associated[random_fruits[0]]) + " tokens!")
+        tokens += tokens_associated[random_fruits[0]]
 
 
 if __name__ == '__main__':
