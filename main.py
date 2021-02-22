cdc3336a0b9d32d74eaad532679df58b7e02e9f3
Eric-Nicolas <randomimus.beakuzz@gmail.com>
2021-02-22 18:38:18 +0100

    Made the script at the first level.

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 # coding: utf-8
+import random
 
 __author__ = 'Eric-Nicolas'
 
+ORANGE = 'orange'
+CHERRY = 'cherry'
+PINEAPPLE = 'pineapple'
+WATERMELON = 'watermelon'
+GOLDEN_APPLE = 'golden apple'
+
 
 def main() -> None:
-    print("Hello World!")
+    print("Welcome to the machine slot GravenFruitiiii!")
+
+    fruits = [ORANGE, CHERRY, PINEAPPLE, WATERMELON, GOLDEN_APPLE]
+    print(random.choice(fruits))
+
+    random_fruits = []
+    while len(random_fruits) < 3:
+        fruit = random.choice(fruits)
+        if fruit not in random_fruits:
+            random_fruits.append(fruit)
+    print(random_fruits)
 
 
 if __name__ == '__main__':
